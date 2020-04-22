4a1d93aa4d162d335fd48b8626e34d6e7ce22f38
CaoKang <caokang@outlook.com>
2020-04-22 15:04:32 +0800

    修改测试样例

--- tests/order.php
+++ tests/order.php
@@ -18,8 +18,10 @@ $request = new BspRequest\OrderRequest();
 $from = $app->config->get('sf')['from'];
 //print_r($from);
 $orderId = 'SDMP'.date('ymdHis').random_int(10000, 99999);
-$request->setHead($app->config->get('sf')['clientcode'])
-    ->getBody()->getOrderRequest()->setOrderid($orderId)
+$req = $request->setHead($app->config->get('sf')['clientcode'])
+    ->getBody()->getOrderRequest();
+
+$req->setOrderid($orderId)
     ->setMailno('SF1021110601405')
     ->setJCompany($from['company'])
     ->setJContact($from['contact'])
@@ -34,8 +36,8 @@ $request->setHead($app->config->get('sf')['clientcode'])
     ->setDTel('13325115789')
     ->setDAddress('山东省济南市历城区东风街道七里铺小区7-2-1');
 
-$request->getBody()->getOrderRequest()->getCargo()->setName("ttt");
-$request->getBody()->getOrderRequest()->getAddedService()->setName("COD")->setValue('10');
+$req->getCargo()->setName("ttt");
+$req->getAddedService()->setName("COD")->setValue('10');
 
 //echo $request;exit;
 //var_dump($app->order);exit;
